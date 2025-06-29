eb6c2c5400ef14fce7a7840379922cdb164d3337
Dom Clayton <dominic.clayton@parkingeye.co.uk>
2025-06-29 20:08:39 +0100

    Updated

--- outputs.tf
+++ outputs.tf
@@ -2,7 +2,7 @@ output "service_principal" {
   value = {
     application_id    = azuread_service_principal.app_registrations.client_id
     object_id         = azuread_service_principal.app_registrations.object_id
-    secret_name       = lower(format("%s", substr(azuread_application.this.display_name, 10, -1)))
+    secret_name       = lower(format("%s", substr(azuread_application.this.display_name, 0, -1)))
     secret_value      = timecmp(time_offset.initial_secret.rfc3339, time_offset.overlapping_secret.rfc3339) == 1 ? azuread_application_password.initial_secret.value : azuread_application_password.overlapping_secret.value
     secret_expiration = timecmp(time_offset.initial_secret.rfc3339, time_offset.overlapping_secret.rfc3339) == 1 ? time_offset.initial_secret.rfc3339 : time_offset.overlapping_secret.rfc3339
   }
