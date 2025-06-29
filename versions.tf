85308cadd8b4ea8b1a97dedae211e1553a94c5b5
Dom Clayton <dominic.clayton@parkingeye.co.uk>
2025-06-29 18:45:33 +0100

    Updated versions

--- versions.tf
+++ versions.tf
@@ -1,13 +1,13 @@
 terraform {
-  required_version = "1.5.5"
+  required_version = "~> 1.0"
   required_providers {
     azuread = {
       source  = "hashicorp/azuread"
-      version = "2.42.0"
+      version = ">= 3.0.0, <= 3.4.0"
     }
     time = {
       source  = "hashicorp/time"
-      version = "0.9.1"
+      version = "~> 0.0"
     }
   }
 }
