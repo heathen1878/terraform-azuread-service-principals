09a833dfce0a43f6067bd7f76fc274e9e534b9a5
Dom Clayton <dominic.clayton@parkingeye.co.uk>
2025-06-29 19:55:20 +0100

    Updated defaults

--- data.tf
+++ data.tf
@@ -1,6 +1,6 @@
 data "azuread_application_published_app_ids" "well_known" {}
 
-data "azuread_client_config" "current" {}
+data "azuread_client_config" "this" {}
 
 resource "azuread_service_principal" "msgraph" {
   client_id    = data.azuread_application_published_app_ids.well_known.result.MicrosoftGraph
